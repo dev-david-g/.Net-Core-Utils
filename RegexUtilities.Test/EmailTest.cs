365422c12f1b5d8cae37c7cf5feb2f2937429221
Gergely David <gergelydavid97@gmail.com>
2017-04-19 16:25:51 +0300

    Initial Commit

---
+++ RegexUtilities.Test/EmailTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utils;
+
+namespace RegexUtilities.Test
+{
+    [TestClass]
+    public class EmailTest
+    {
+        [TestMethod]
+        public void isValidEmailMethodReturnTrueIfMailIsValidWithDomain()
+        {
+            //Arrange
+            var expected = true;
+            var actual = false;
+            var canditatingEmail = "email@emai.com";
+
+            //Act
+            actual = Email.IsValidEmail(canditatingEmail);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void isValidEmailMethodReturnTrueIfMailIsValidWithIPAddress()
+        {
+            //Arrange
+            var expected = true;
+            var actual = false;
+            var canditatingEmail = "email@[192.168.1.27]";
+
+            //Act
+            actual = Email.IsValidEmail(canditatingEmail);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void isValidEmailMethodReturnFalseIfMailIsFalse()
+        {
+            //Arrange
+            var expected = false;
+            var actual = false;
+            var canditatingEmail = "email@.com";
+
+            //Act
+            actual = Email.IsValidEmail(canditatingEmail);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Utils/Email.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Utils
+{
+    public static class Email
+    {
+        public static bool IsValidEmail(string candidatingEmail)
+        {
+            string regexPattern = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+
+            if (String.IsNullOrWhiteSpace(candidatingEmail))
+                return false;
+
+            try
+            {
+                return Regex.IsMatch(candidatingEmail, regexPattern, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+}
